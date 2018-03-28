9a6139c15974624fdc4243482a5f3d564d469d29
satyajeetnim <snimgaon@cisco.com>
2018-03-27 22:25:24 -0700

    satyajeet: Added public module to blueprints. (#7)
    Change-Id: Iabd890ba924752438ead388c80dd2d503a193468

--- panorama/public/views.py
+++ panorama/public/views.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-module = Blueprint('public', __name__)
+module = Blueprint('public', __name__, template_folder='pages')
 
 @module.route('/')
 def home():
-    return 'Home'
+    return render_template('index.html')
+
+
+@module.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@module.route('/contact')
+def contact():
+    return render_template('contact.html')
