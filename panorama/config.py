0dbdf8808cc8c3cfe6d48a4d801ed4c70465a2bf
Satyajeet Nimgaonkar <snimgaon@cisco.com>
2018-03-29 22:12:43 -0700

    satyajeet: Fixed Blueprint, added flask security and templating.
    Change-Id: I9a67b52626a54afb44557083c99901dd804abf01

---
+++ panorama/config.py
@@ -0,0 +1,33 @@
+import uuid
+
+
+class Config(object):
+    def __init__(self):
+        self.SECRET_KEY = uuid.uuid4().hex
+
+        # MONGODB configs
+        self.MONGODB_DB = 'panorama'
+        self.MONGODB_HOST = 'localhost'
+        self.MONGODB_PORT = 27017
+
+        # GMAIL SMTP configs
+        self.DEFAULT_MAIL_SENDER = 'Admin < satyajeetnim@gmail.com >'
+        self.MAIL_SERVER = 'smtp.gmail.com'
+        self.MAIL_PORT = 465
+        self.MAIL_USE_SSL = True
+        self.MAIL_USERNAME = 'atyajeetnim@gmail.com'
+        self.MAIL_PASSWORD = 'jeet&puj0302'
+
+        # Flask-Security configs
+        self.SECURITY_EMAIL_SENDER = 'Security < security@example.com >'
+        self.SECURITY_LOGIN_WITHOUT_CONFIRMATION = True
+        self.SECURITY_REGISTERABLE = True
+        self.SECURITY_RECOVERABLE = True
+        #self.SECURITY_URL_PREFIX = '/auth'
+        self.SECUIRTY_POST_LOGIN = '/'
+        self.SECURITY_PASSWORD_HASH = 'sha512_crypt'
+        self.SECURITY_PASSWORD_SALT = uuid.uuid4().hex
+        self.SECURITY_CONFIRMABLE = True
+        self.SECURITY_LOGIN_WITHOUT_CONFIRMATION = False
+
+panorama_config = Config()
--- panorama/managers/views.py
+++ panorama/managers/views.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 module = Blueprint('property-managers', __name__)
 
-@module.route('/manager')
-def home():
+@module.route('/manager-login')
+def manager_login():
     return 'Welcome to Property Managers Page'
