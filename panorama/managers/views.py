01cdf12a04330d966731e52b955895866a8a2228
satyajeetnim <snimgaon@cisco.com>
2018-04-04 21:50:05 -0700

    satyajeet: Deleting unwanted files. (#14)
    Change-Id: I2adce5212bca02f77f024b843c9ef1d6c10f01b7

--- panorama/managers/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-module = Blueprint('property-managers', __name__)
-
-@module.route('/manager-login')
-def manager_login():
-    return 'Welcome to Property Managers Page'
